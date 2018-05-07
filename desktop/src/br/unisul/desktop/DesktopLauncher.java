9a8660e33feb098c0d9f73b2ffbf0dab065528a3
Patrick Machado <paamachado.93@gmail.com>
2018-05-07 20:27:06 -0300

    Project generation

---
+++ desktop/src/br/unisul/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.unisul.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.unisul.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
